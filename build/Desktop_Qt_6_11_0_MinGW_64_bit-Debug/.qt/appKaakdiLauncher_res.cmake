06fc2d2165ff9f9e2f3957d65c79674b285ce908
tarang-soni <83337255+tarang-soni@users.noreply.github.com>
2026-06-15 12:29:54 +0530

    removed incorrect folders from git

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/appKaakdiLauncher_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appKaakdiLauncher")
-set(working_dir "D:/Dev/Projects/qt/KaakdiLauncher")
-set(src_and_dest_list
-    "D:/Dev/Projects/qt/KaakdiLauncher/Resources.qrc"
-    "D:/Dev/Projects/qt/KaakdiLauncher/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/KaakdiLauncher/Resources.qrc"
-
-)
-set(timestamp_file "D:/Dev/Projects/qt/KaakdiLauncher/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/appKaakdiLauncher_res.txt")
